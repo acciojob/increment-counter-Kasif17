243ae07e5a0525d59feaac378743398ee40fb074
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-08-15 17:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let count = 0;
+let counterElemnet = document.getElementById("text");
+function Updatecounter() {
+	counterElemnet.textContent = count;
+}
+function Increment() {
+	count++;
+	Updatecounter()
+}
+Increment()
