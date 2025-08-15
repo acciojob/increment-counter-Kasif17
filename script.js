88141392a1469e17c6182552ff5183adfa38f7ef
Mohd Kasif Khan <developer786kasif@gmail.com>
2025-08-15 19:00:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 let count = 0;
-let counterElement = document.getElementById("text");
+let counterElement = document.getElementById("counter");
 
 function Updatecounter() {
     counterElement.textContent = count;
 }
 
-function Increment() {
+function incrementBtn() {
     alert(count);
     count++;
     Updatecounter();
